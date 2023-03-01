e168b8a10843d913c00e2fe7e2aef26cf36fd284
ldandoy <ldandoy@overconsulting.net>
2023-03-01 12:24:53 +0100

    MAJ

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ dire_aurevoir('test')*/
 import express from 'express';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
+import helmet from 'helmet';
+import session from 'express-session'
 
 
 mongoose.connect(`mongodb://root:root@127.0.0.1:27017/`, {
@@ -23,16 +25,28 @@ mongoose.connect(`mongodb://root:root@127.0.0.1:27017/`, {
 });
 
 import motivateRoute from './routes/motivate.js';
+import authRoute from './routes/auth.js';
 
 const App = express();
+App.use(helmet());
 App.use(morgan('dev'));
 App.use(express.json());
+App.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+        httpOnly: true,
+        secure: false
+    }
+}))
 
 App.get('/',(request, response) => {
-    return response.send('<h1>it works !</h1>');
+    return response.send('<h1>It works !</h1>');
 });
 
 App.use('/api/motivate', motivateRoute);
+App.use('/api', authRoute);
 
 const PORT = 5663;
 
---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+import express from 'express';
+
+const Router = express.Router();
+
+Router.post('/login', (req, res) => {
+    const {email, password} = req.body;
+
+    if (email == "admin" && password == "admin") {
+        req.session.user = {
+            email
+        }
+        return res.status(200).json(req.session.user);
+    }
+
+    return res.status(500).json({msg: "Il y a eu une erreur lors de l'authentification !"});
+});
+
+Router.get('/me', (req, res) => {
+    if (req.session.user) {
+        return res.status(200).json(req.session.user);
+    }
+
+    return res.status(500).json({msg: "Vous n'est pas connecté !"});
+})
+
+Router.get('/logout', (req, res) => {
+    if (req.session.user) {
+        req.session.destroy();
+    }
+
+    return res.status(200).json({msg: "Vous est déconnecté !"});
+})
+
+export default Router;
